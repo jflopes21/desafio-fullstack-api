da44855c975c0d4f99d0ea366f38050bbcc463f2
Joao Francisco <joaofrancisco21@yahoo.com.br>
2024-04-13 20:25:56 -0300

    Add: Patch Route

--- src/routes/developers.routes.ts
+++ src/routes/developers.routes.ts
@@ -8,6 +8,7 @@ const developersRouter = Router();
 developersRouter.get("/", developersController.index);
 developersRouter.post("/", developersController.create);
 developersRouter.put("/:id", developersController.update);
+developersRouter.patch("/:id", developersController.update);
 developersRouter.delete("/:id", developersController.delete);
 
 export { developersRouter };
